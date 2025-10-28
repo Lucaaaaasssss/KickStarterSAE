3f4662bf6f928bf12c1760e86c95611aa4eaa207
Lucaaaaasssss <lucaslebecq15@gmail.com>
2025-10-28 12:39:49 +0100

    viteconfigupdate

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',           // par défaut sur un domaine dédié
+  base: '/kickstarter/',   // ⬅️ important : avec slash au début ET à la fin
   build: { outDir: 'dist' }
 })
