ca6a74f0ef9d2dd0cac5153f33ea71bc052c483d
Lucas Lebecq <lucaslebecq15@gmail.com>
2025-10-28 12:42:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',           // par défaut sur un domaine dédié
+  base: '/kickstarter/',   
   build: { outDir: 'dist' }
 })
