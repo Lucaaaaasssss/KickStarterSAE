773edaf476a762054aa370ddd58d54638c88e37a
Lucaaaaasssss <lucaslebecq15@gmail.com>
2025-11-07 14:53:54 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <HeaderComponent />
   <SectionShowcaseComponent />
   <SectionDescriptionComponent />
+  <FooterComponent />
 </template>
 
 <script setup>
 import HeaderComponent from './components/Header.vue'
 import SectionShowcaseComponent from './components/SectionShowcase.vue'
 import SectionDescriptionComponent from './components/SectionDescription.vue'
+import FooterComponent from './components/Footer.vue'
 
 
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer>
+    <p>© 2025 Kickstarter. Tous droits réservés. | Mathias Packo & Lucas Lebecq </p>
+  </footer>
+</template> <script setup> </script>
+<style scoped>
+footer { background-color: #f8f9fb; border-top: 1px solid #e2e2e2; color: black; text-align: center; padding: 15px 10px; font-size: 14px; } footer p { margin: 0; }
+</style>
